93cfd56f012817485bad801b9f92c4bd3fd46307
Shrinivas Sherikar <ssherikar2005@gmail.com>
2025-07-26 01:03:36 +0530

    landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
-const {heroui} = require('@heroui/theme');
+
 // tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    
   ],
   theme: {
   	extend: {
@@ -67,5 +67,5 @@ export default {
 	
   },
   darkMode: ["class", "class"],
-  plugins: [heroui(), require("tailwindcss-animate")],
+  plugins: [ require("tailwindcss-animate")],
 };
